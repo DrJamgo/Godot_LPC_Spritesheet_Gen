6e47bebb65e75b410fe53ae0328dcc1692186eec
DrJamgo <drjamgo@hotmail.com>
2022-03-19 16:31:39 +0100

    REF

--- LPCSpriteFactory.gd
+++ LPCSpriteFactory.gd
@@ -1,88 +1,24 @@
-extends Node
+tool
+extends Resource
 class_name LPCSpriteFactory
 
-var all_json_list := {}
+export(Dictionary) var all_json_list := {}
 var all_json_tree := {}
 var all_typename_list := {}
 var selected_typename_list := {}
 
 export(String) var rootpath = self.get_script().get_path().get_base_dir() + "/lpc_spritesheets/"
 export(String) var dest_path = "res://assets/lpc_spritesheets/"
+export(bool) var reload_json_files  = false setget _reload_json_files
 
-export(String, 'male', 'female', 'child', 'pregnant', 'muscular') var body_type := 'male'
+#
+# PUBLIC
+# 
 
-func _init():
-    all_json_list = _loadJsonFiles()
-    all_json_tree = _getNodes(all_json_list.keys(), "")
-    pass
-    
-func _loadJsonFiles():
-    print("_loadJsonFiles")
-    var dict = {}
+func get_tree(body_type : String):
+    return _getNodes(body_type, all_json_list.keys(), "")
 
-    var dir = Directory.new()
-    var definitions_path = rootpath + "/sheet_definitions/"
-    if dir.open(definitions_path) == OK:
-        dir.list_dir_begin(true, true)
-        var file_name = dir.get_next()
-        while file_name != "":
-            if !dir.current_is_dir() and file_name.ends_with('.json'):
-                var file = File.new()
-                file.open(definitions_path + file_name, file.READ);
-                var text = file.get_as_text()
-                var json_result = JSON.parse(text)
-                file.close()
-                if json_result.error == OK:
-                    dict[file_name.get_basename().replace('_','/')] = json_result.result
-
-            file_name = dir.get_next()
-    print(String(dict.keys().size()) + " JSON files found")
-    return dict
-
-func _getNodes(keys: Array, location: String):
-    var list = {}
-    while keys.size() != 0:
-        var key = keys[0]
-        var last_ = key.find_last('/')
-        var nodename = key.substr(last_+1)
-        if key.begins_with(location) and location.length() > last_:
-            ## corrent place
-            var skip = false
-            if all_json_list.has(key):
-                var json_data = all_json_list[key]
-                var typename = json_data.type_name
-                if not typename in all_typename_list:
-                    all_typename_list[typename] = null
-                if !json_data.layer_1.has(body_type):
-                    skip = true
-            if !skip:
-                list[nodename] = key
-            keys.remove(0)
-
-        elif key.begins_with(location):
-            ## lets add a level
-            var new_location = String(location)
-            var numLevels = new_location.split('/',true).size()-1
-            var new_level = key.split('/')[numLevels] + "/"
-            if numLevels > 0:
-                new_location += new_level
-            else:
-                new_location = new_level
-            var children = _getNodes(keys, new_location)
-            if children:
-                list[new_level] = children
-        else:
-            ## too far, lets exit
-            match list.keys().size():
-                0:
-                    return null
-                #1:
-                #    return list[list.keys()[0]]
-                _:
-                    return list
-    return list
-
-func get_lpc_layers(base : String, variant : String) -> Array:
+func get_lpc_layers(body_type : String, base : String, variant : String) -> Array:
     var layers := []
     var json_data : Dictionary = all_json_list[base]
     var layer_idx := 1
@@ -113,3 +49,80 @@ func load_texture(layer : LPCSpriteLayer) -> bool:
         dir.copy(rootpath + 'spritesheets/' + layer.rel_path, layer.abs_path)
         
     return resource_exists
+
+#
+# PRIVATE
+# 
+
+func _reload_json_files(_reload : bool):
+    all_json_list = _loadJsonFiles()
+    set_deferred("reload", false)
+
+func _init():
+    _reload_json_files(true)
+
+func _loadJsonFiles():
+    print("_loadJsonFiles")
+    var dict = {}
+
+    var dir = Directory.new()
+    var definitions_path = rootpath + "/sheet_definitions/"
+    if dir.open(definitions_path) == OK:
+        dir.list_dir_begin(true, true)
+        var file_name = dir.get_next()
+        while file_name != "":
+            if !dir.current_is_dir() and file_name.ends_with('.json'):
+                var file = File.new()
+                file.open(definitions_path + file_name, file.READ);
+                var text = file.get_as_text()
+                var json_result = JSON.parse(text)
+                file.close()
+                if json_result.error == OK:
+                    dict[file_name.get_basename().replace('_','/')] = json_result.result
+
+            file_name = dir.get_next()
+    print(String(dict.keys().size()) + " JSON files found")
+    return dict
+
+func _getNodes(body_type : String, keys: Array, location: String):
+    var list = {}
+    while keys.size() != 0:
+        var key = keys[0]
+        var last_ = key.find_last('/')
+        var nodename = key.substr(last_+1)
+        if key.begins_with(location) and location.length() > last_:
+            ## current place
+            var skip = false
+            if all_json_list.has(key):
+                var json_data = all_json_list[key]
+                var typename = json_data.type_name
+                if not typename in all_typename_list:
+                    all_typename_list[typename] = null
+                if !json_data.layer_1.has(body_type):
+                    skip = true
+            if !skip:
+                list[nodename] = key
+            keys.remove(0)
+
+        elif key.begins_with(location):
+            ## lets add a level
+            var new_location = String(location)
+            var numLevels = new_location.split('/',true).size()-1
+            var new_level = key.split('/')[numLevels] + "/"
+            if numLevels > 0:
+                new_location += new_level
+            else:
+                new_location = new_level
+            var children = _getNodes(body_type, keys, new_location)
+            if children:
+                list[new_level] = children
+        else:
+            ## too far, lets exit
+            match list.keys().size():
+                0:
+                    return null
+                #1:
+                #    return list[list.keys()[0]]
+                _:
+                    return list
+    return list
--- sprite_layer.shader
+++ sprite_layer.shader
@@ -1,7 +1,7 @@
 shader_type canvas_item;
 render_mode blend_mix;
 
-uniform vec2 outLineSize  = vec2(0.00118,0.000744);
+uniform vec2 outLineSize  = vec2(1,1);
 uniform vec4 outLineColor = vec4(1.0, 1.0, 1.0, 0.0);
 uniform vec4 mixColor     = vec4(1.0, 1.0, 1.0, 0.0);
 
@@ -13,7 +13,7 @@ void fragment()
     {
         for(float y = -1.0; y <= 1.0; y+=1.0) {
             for(float x = -1.0; x <= 1.0; x+=1.0) {
-                vec2 offset = vec2(x,y) * outLineSize;
+                vec2 offset = vec2(x,y) * outLineSize * TEXTURE_PIXEL_SIZE;
                 vec4 c = texture(TEXTURE, UV + offset);
                 if(c.a  == 1.0 && any(notEqual(c.rgb, vec3(0.0, 0.0, 0.0)))) {
                     doReplace = true;
